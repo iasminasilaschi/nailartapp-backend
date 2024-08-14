b415197be8a2ce83e27c365f8bd1be66db3dc999
iasminasilaschi <iasminasilaschi@yahoo.com>
2024-08-14 20:21:19 +0300

    Fixed Id issue: generated by db

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class User
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public string Username { get; set; }
     public string PasswordHash { get; set; }
