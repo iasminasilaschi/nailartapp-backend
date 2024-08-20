7f1d3e12198661c03812b09ae80f32f08506edbc
iasminasilaschi <iasminasilaschi@yahoo.com>
2024-08-20 17:55:33 +0300

    Added model and service for appointments

---
+++ NailArt/Models/Appointment.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NailArtApp.Models
+{
+    public class Appointment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("time")]
+        public string Time { get; set; }
+
+        [BsonElement("clientId")]
+        public string ClientId { get; set; }
+
+        [BsonElement("artistId")]
+        public string ArtistId { get; set; }
+
+        [BsonElement("serviceType")]
+        public string ServiceType { get; set; }
+
+        [BsonElement("notes")]
+        public string Notes { get; set; }
+    }
+}
---
+++ NailArt/Services/AppointmentService.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+using NailArtApp.Models;
+
+namespace NailArtApp.Services
+{
+    public class AppointmentService
+    {
+        private readonly IMongoCollection<Appointment> _appointmentsCollection;
+
+        public AppointmentService(IMongoDatabase database)
+        {
+            _appointmentsCollection = database.GetCollection<Appointment>("Appointments");
+        }
+    }
+
+}
